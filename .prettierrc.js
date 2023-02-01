1366d37b100f1ded4f91cf6de568f3edb2b946cd
zhangtianyi <1622339443@qq.com>
2023-02-01 11:49:34 +0800

    feat: 按需加载vant

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  printWidth: 100, // 单行代码字符数量
+  tabWidth: 2, // tab使用两个空格
+  useTabs: false, // 不使用制表符缩进，使用空格缩进
+  semi: false, // 不需要分号结束
+  singleQuote: true, // 使用单引号
+  bracketSpacing: true, // 对象左右两侧都需要空格
+};
---
+++ src/views/tabs/TabsView.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+// 记录当前选择的tab索引
+const active = ref('home')
+</script>
+
+<template>
+  <div class="center">{{ active }}</div>
+  <van-tabbar v-model="active">
+    <van-tabbar-item name="home" icon="home-o">首页</van-tabbar-item>
+    <van-tabbar-item name="order" icon="bars">订单</van-tabbar-item>
+    <van-tabbar-item name="mine" icon="contact">我的</van-tabbar-item>
+  </van-tabbar>
+</template>
+
+<style scoped>
+.center {
+  text-align: center;
+}
+</style>
