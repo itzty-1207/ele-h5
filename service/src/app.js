6097144d26bd66d81d2d84abc06679dc88501bba
zhangtianyi <1622339443@qq.com>
2023-02-01 17:42:33 +0800

    feat: 新增json-server

---
+++ service/src/app.js
@@ -0,0 +1,24 @@
+const jsonServer = require('json-server');
+const path = require('path');
+const router = require('./router');
+const db = require('./db')();
+
+const server = jsonServer.create();
+
+const middlewares = jsonServer.defaults({
+  static: path.join(__dirname, '../public'),
+});
+
+server.use(middlewares);
+
+server.use(jsonServer.bodyParser);
+
+router(server);
+
+const jsonRouter = jsonServer.router(db);
+
+server.use('/api', jsonRouter);
+
+server.listen(8000, () => {
+  console.log('JSON Server is running at 8000');
+});
